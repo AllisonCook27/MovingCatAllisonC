c17dad97ee580918865afeb8bfc65ac9f9b44846
AllisonCook27 <allison.cook@ocsbstudent.ca>
2018-02-16 12:46:09 -0500

    Initial commit

---
+++ MovingCatForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace MovingCatAllisonC
+{
+    partial class frmMovingCat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCat = new System.Windows.Forms.PictureBox();
+            this.mnuMenu = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniCat2 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picCat)).BeginInit();
+            this.mnuMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picCat
+            // 
+            this.picCat.Location = new System.Drawing.Point(65, 58);
+            this.picCat.Name = "picCat";
+            this.picCat.Size = new System.Drawing.Size(136, 149);
+            this.picCat.TabIndex = 0;
+            this.picCat.TabStop = false;
+            // 
+            // mnuMenu
+            // 
+            this.mnuMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile,
+            this.mniCat});
+            this.mnuMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMenu.Name = "mnuMenu";
+            this.mnuMenu.Size = new System.Drawing.Size(284, 24);
+            this.mnuMenu.TabIndex = 1;
+            this.mnuMenu.Text = "Menu";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // mniCat
+            // 
+            this.mniCat.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniCat1,
+            this.mniCat2});
+            this.mniCat.Name = "mniCat";
+            this.mniCat.Size = new System.Drawing.Size(37, 20);
+            this.mniCat.Text = "Cat";
+            // 
+            // mniCat1
+            // 
+            this.mniCat1.Name = "mniCat1";
+            this.mniCat1.Size = new System.Drawing.Size(152, 22);
+            this.mniCat1.Text = "Cat 1";
+            this.mniCat1.Click += new System.EventHandler(this.mniCat1_Click);
+            // 
+            // mniCat2
+            // 
+            this.mniCat2.Name = "mniCat2";
+            this.mniCat2.Size = new System.Drawing.Size(152, 22);
+            this.mniCat2.Text = "Cat 2";
+            this.mniCat2.Click += new System.EventHandler(this.mniCat2_Click);
+            // 
+            // frmMovingCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.picCat);
+            this.Controls.Add(this.mnuMenu);
+            this.MainMenuStrip = this.mnuMenu;
+            this.Name = "frmMovingCat";
+            this.Text = "Moving Cat by Allison C";
+            this.Load += new System.EventHandler(this.frmMovingCat_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picCat)).EndInit();
+            this.mnuMenu.ResumeLayout(false);
+            this.mnuMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCat;
+        private System.Windows.Forms.MenuStrip mnuMenu;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ToolStripMenuItem mniCat;
+        private System.Windows.Forms.ToolStripMenuItem mniCat1;
+        private System.Windows.Forms.ToolStripMenuItem mniCat2;
+    }
+}
+
---
+++ MovingCatForm.cs
@@ -0,0 +1,47 @@
+﻿/*
+ * Created by: Allison Cook
+ * Created on: 17 February, 2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #8 - Moving Cat
+ * This program displays a different image in a picture box when a menu item is clicked
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingCatAllisonC
+{
+    public partial class frmMovingCat : Form
+    {
+        public frmMovingCat()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMovingCat_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mniCat1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mniCat2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
